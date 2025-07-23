66df9f553ee2a58acf15d6d034e6eca8c1caa49e
Andrew Yoon <126834174+AndrewBotics@users.noreply.github.com>
2025-07-23 18:49:24 -0500

    have you ever thought about how mao sounds like meow

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-const slides = document.querySelectorAll('.slide');
-
-function isInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  const buffer = 25;
-  return rect.top < window.innerHeight - buffer && rect.bottom > buffer;
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.classList.toggle("show");
 }
-
-function handleScroll() {
-  slides.forEach(el => {
-    const fromLeft = el.classList.contains('slide-left');
-    const fromRight = el.classList.contains('slide-right');
-
-    if (isInViewport(el)) {
-      el.classList.add('visible');
-      el.classList.remove('exit-left', 'exit-right');
-    } else {
-      el.classList.remove('visible');
-      if (fromLeft) el.classList.add('exit-left');
-      if (fromRight) el.classList.add('exit-right');
-    }
-  });
-}
-
-window.addEventListener('scroll', handleScroll);
-window.addEventListener('resize', handleScroll);
-window.addEventListener('DOMContentLoaded', handleScroll);
